1e687c18697e0ac05c63e6731872b7f2ca5ad43d
GautierWiann <wianni.gautier@gmail.com>
2024-01-08 15:14:42 +0100

    public

--- components/stopwatch.js
+++ components/stopwatch.js
@@ -1,29 +1,43 @@
 import styles from '../styles/stopwatch.module.css'
-import { useState ,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Row, Col } from 'reactstrap'
 
 export default function Stopwatch(props) {
-  
+
   let userId = useSelector(state => state.user.value._id)
   const [isActive, setIsActive] = useState(false);
   const [isPaused, setIsPaused] = useState(true);
   const [time, setTime] = useState(0);
+  console.log("🚀 ~ file: stopwatch.js:12 ~ Stopwatch ~ time:", time)
   const [rsum, setRsum] = useState(0)
-  console.log("🚀 ~ file: stopwatch.js:9 ~ Stopwatch ~ time:", time)
   const [sessionId, setSessionId] = useState("")
+  const [durations, setDurations] = useState([])
+  const [start, setStart] = useState()
+
+
+function calculTimePassed() {
+  const timeDifferenceInMilliseconds = new Date(Date.now()) - start;
+ return (timeDifferenceInMilliseconds / (1000 * 60 * 60));
+ }
 
   useEffect(() => {
     let interval = null;
     let real = 0
-      for (const session of props.task.sessions) {
-          real += session.duration
-      }
-      real +=time/ 3600000 
-      setRsum(real)
+    for (const session of props.task.sessions) {
+      real += session.duration
+    }
+    real += time / 3600000
+    setRsum(real)
     if (isActive && isPaused === false) {
       interval = setInterval(() => {
-        setTime((time) => time + 10);
+        let dur = 0
+        if (durations[0]){
+        for (const duration of durations) {
+          dur += duration
+        }}
+        dur += calculTimePassed()
+        setTime(dur);
       }, 10);
     } else {
       clearInterval(interval);
@@ -32,35 +46,42 @@ export default function Stopwatch(props) {
     return () => {
       clearInterval(interval);
     };
- 
-   
+
+
   }, [isActive, isPaused]);
   useEffect(() => {
     let real = 0
     for (const session of props.task.sessions) {
-        real += session.duration
+      real += session.duration
     }
-    real +=time/ 3600000 
+    real += time / 3600000
     setRsum(real)
-  },[props])
+  }, [props])
 
   const handleStart = async () => {
+    setStart(new Date(Date.now()))
     setIsActive(true);
     setIsPaused(false);
     const rawResponse = await fetch('https://noel-back.vercel.app/task/add-session', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ taskId: props.task._id, projectId: props.projectId, userId: userId, session:{}  }),
-  });
+      body: JSON.stringify({ taskId: props.task._id, projectId: props.projectId, userId: userId, session: {} }),
+    });
 
-  const response = await rawResponse.json(); 
-  if(response.result) {
+    const response = await rawResponse.json();
+    if (response.result) {
       //notification good
       setSessionId(response.sessionId)
-  }
+    }
   };
-
+ 
   const handlePauseResume = () => {
+    if (!isPaused) {
+      setDurations([...durations, calculTimePassed()])
+    } else {
+      setStart(new Date(Date.now()))
+    }
+ 
     setIsPaused(!isPaused);
   };
   const handleFinish = async () => {
@@ -68,15 +89,15 @@ export default function Stopwatch(props) {
     const rawResponse = await fetch('https://noel-back.vercel.app/task/end-session', {
       method: 'PATCH',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ taskId: props.task._id, projectId: props.projectId, userId: userId, sessionId, duration: time/ 3600000  }),
-  });
+      body: JSON.stringify({ taskId: props.task._id, projectId: props.projectId, userId: userId, sessionId, duration: time }),
+    });
 
-  const response = await rawResponse.json(); 
-  if(response.result) {
-    setTime(0)
-    setSessionId("")
+    const response = await rawResponse.json();
+    if (response.result) {
+      setTime(0)
+      setSessionId("")
       //notification good
-  }
+    }
   }
 
   const StartButton = (
@@ -99,32 +120,32 @@ export default function Stopwatch(props) {
   return (
     <div id={styles.container}>
       <Row>
-        <Col md={{ size: 6 }} style={{marginTop:'5vh'}} className={styles.taskCard}>
+        <Col md={{ size: 6 }} style={{ marginTop: '5vh' }} className={styles.taskCard}>
           {/* <Row>
           <Col md="7"> */}
-            <p className={styles.title}>{props.task.name}</p>
-            <p className={styles.desc}>{props.task.desc}</p>
+          <p className={styles.title}>{props.task.name}</p>
+          <p className={styles.desc}>{props.task.desc}</p>
           {/* </Col> */}
           {/* <Col md="4" style={{ textAlign: 'right' }}> */}
-            <p>budget restant : {Math.floor(props.task.budget - rsum)}h{(Math.round(((props.task.budget - rsum) % 1) * 60) != 0)&&Math.round(((props.task.budget - rsum) % 1) * 60)}</p>
-              
+          <p>budget restant : {Math.floor(props.task.budget - rsum)}h{(Math.round(((props.task.budget - rsum) % 1) * 60) != 0) && Math.round(((props.task.budget - rsum) % 1) * 60)}</p>
+
           {/* </Col>
           </Row> */}
-       </Col>
-          <Col md={{ size: 5, offset: 1 }} >
-            <div className={styles.timerCol}>
-              <div className={styles.timer}>
-                <span className="digits">
-                  {("0" + Math.floor((time / 3600000) % 60)).slice(-2)}:
-                </span>
-                <span className="digits">
-                  {("0" + Math.floor((time / 60000) % 60)).slice(-2)}
-                </span>
-              </div>
-              <div className={styles.buttonsDiv}>
-                {isActive ? ActiveButtons : StartButton}
-              </div></div>
-          </Col></Row>
+        </Col>
+        <Col md={{ size: 5, offset: 1 }} >
+          <div className={styles.timerCol}>
+            <div className={styles.timer}>
+              <span className="digits">
+                {("0" + Math.floor((time) % 60)).slice(-2)}:
+              </span>
+              <span className="digits">
+                {("0" + Math.floor((time * 60) % 60)).slice(-2)}
+              </span>
+            </div>
+            <div className={styles.buttonsDiv}>
+              {isActive ? ActiveButtons : StartButton}
+            </div></div>
+        </Col></Row>
     </div>
   );
 }
--- components/taskCard.js
+++ components/taskCard.js
@@ -47,15 +47,24 @@ export default function TaskCard(props) {
     <div className={styles.container} id={styles.taskContainer} style={style}>
       {props.show &&
         <div>
-          <img className={styles.icon} id={checkStyle}  src='check.png' onClick={() => {handleFinish()}}/>
-          <div onClick={() => props.handleDelete()}><svg className={styles.icon} id={styles.trash}  xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z" /></svg></div></div>}
-          <div className={styles.notiglow}>
-          
-        </div>
+          <img className={styles.icon} id={checkStyle} src='check.png' onClick={() => { handleFinish() }} />
+          <div onClick={() => props.handleDelete()}><svg className={styles.icon} id={styles.trash} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z" /></svg></div></div>}
+      <div className={styles.notiglow}>
+
+      </div>
       <div className={styles.notiborderglow}></div>
       <Row style={{ margin: '0 10px 0 2px', justifyContent: 'space-between' }}>
         <div className={styles.notititle}>{props.task.name}</div>
-        <div className={styles.notititle}> {Math.floor(sumTime)} h {(Math.round(((sumTime) % 1) * 60) != 0)&&Math.round(((sumTime) % 1) * 60)} sur {Math.floor(props.task.budget)}h{(Math.round(((props.task.budget) % 1) * 60) != 0)&&Math.round(((props.task.budget) % 1) * 60)}</div>
+        <div className={styles.notititle}>
+          {Math.floor(sumTime)} h  {' '}
+          {((Math.round(((sumTime) % 1) * 60) !== 0) && Math.round(((sumTime) % 1) * 60) < 10)
+            ? `0${Math.round(((sumTime) % 1) * 60)}`
+            : Math.round(((sumTime) % 1) * 60)}{' '}
+          sur {Math.floor(props.task.budget)}h{' '}
+          {((Math.round(((props.task.budget) % 1) * 60) !== 0) && Math.round(((props.task.budget) % 1) * 60) < 10)
+            ? `0${Math.round(((props.task.budget) % 1) * 60)}`
+            : Math.round(((props.task.budget) % 1) * 60)}
+        </div>
       </Row>
       {props.show && <div className={styles.notibody}>{props.task.desc}</div>}
     </div>
